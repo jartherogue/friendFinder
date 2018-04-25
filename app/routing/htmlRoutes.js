0dca194f1e28fcd7520c0f3820e2551449033e7b
jartherogue <jasreev@gmail.com>
2018-04-24 23:25:42 -0600

    committing all files

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,16 @@
+//dependencies
+var path = require("path");
+var express = require('express');
+
+module.exports = function(app) {
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+  });
+  app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/survey.html'"));
+  });
+//Send home if no route is found.
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+  });
+};
