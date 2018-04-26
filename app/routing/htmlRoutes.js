c5c3ab5329b6914f91af00f65a02cccd8bbb728f
jartherogue <jasreev@gmail.com>
2018-04-25 20:42:38 -0600

    changed dir to try to get it to work

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -4,9 +4,9 @@ var express = require('express');
 
 module.exports = function(app) {
   app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/home.html"));
+    res.sendFile(path.join(__dirname, "./public/home.html"));
   });
   app.get("/survey", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/survey.html'"));
+    res.sendFile(path.join(__dirname, "./public/survey.html'"));
   });
 };
